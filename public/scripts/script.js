0ed59a3cba485565506d23a71d897afdb13d2c1b
Jesse Kramer <jessekramer15@gmail.com>
2024-06-05 14:58:45 +0200

    JK - Carousel try V1

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,23 @@
 // Scripts
+const items = document.querySelectorAll('.carousel-item');
+const carouselPrev = document.querySelectorAll('.carousel-btn.next')
+const carouselNext = document.querySelectorAll('.carousel-btn.next')
+let currentItem = 0;
+
+function showItem(index) {
+    items.forEach((item, i) => {
+        item.classList.toggle('active', i === index);
+    });
+}
+
+carouselPrev.addEventListener('click', () => {
+    currentItem = (currentItem > 0) ? currentItem - 1 : items.length - 1;
+    showItem(currentItem);
+});
+
+carouselNext.addEventListener('click', () => {
+    currentItem = (currentItem + 1) % items.length;
+    showItem(currentItem);
+});
+
+showItem(currentItem);
