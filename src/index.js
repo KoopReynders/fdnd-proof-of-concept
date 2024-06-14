28fa4646b24967e7932aac50a02af617298cfa34
Jesse Kramer <jessekramer15@gmail.com>
2024-06-14 10:15:26 +0200

    JK - Disabled charts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Chart from './components/Chart.js';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import Chart from './components/Chart.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('chart-container');
-    if (container) {
-      const root = createRoot(container);
-      root.render(<Chart />);
-    } else {
-      console.error('Could not find the container element to mount the React component.');
-    }
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const container = document.getElementById('chart-container');
+//     if (container) {
+//       const data = window.scanData || [];
+//       const root = createRoot(container);
+//       root.render(<Chart data={data} />);
+//     } else {
+//       console.error('Could not find the container element to mount the React component.');
+//     }
+// });
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,24 +1,24 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public', 'scripts'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  mode: 'development',
-};
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'public', 'scripts'),
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+//   mode: 'development',
+// };
